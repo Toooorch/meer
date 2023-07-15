a1cd49b96de6dd19a2d26620384ba346f043572d
Michal Soukup <michal@michalsoukup.com>
2023-07-15 18:48:54 +0200

    Update buybutton.js

--- buybutton.js
+++ buybutton.js
@@ -491,7 +491,45 @@ switch (href) {
 
   default:
 
-    // Czech
+      if (deliveryDateDesktop !== null) {
+        // Czech
+        var tomorrow = "zítra u Vás (Zásilkovna)";
+        var nexttomorrow = "pozítří u Vás (Zásilkovna)";
+        var monday = "v pondělí u Vás (Zásilkovna)";
+        var tuesday = "v úterý u Vás (Zásilkovna)";
+
+        if (thehours >= 0 && thehours < 19 && dayOfWeek == 1) {
+            deliveryMessage = tomorrow;
+        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 1) {
+            deliveryMessage = nexttomorrow;
+        } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 2) {
+            deliveryMessage = tomorrow;
+        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 2) {
+            deliveryMessage = nexttomorrow;
+        } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 3) {
+            deliveryMessage = tomorrow;
+        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 3) {
+            deliveryMessage = nexttomorrow;
+        } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 4) { // Thursday before 19
+            deliveryMessage = tomorrow;
+        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 4) { // Thursday after 19
+            deliveryMessage = monday;
+        } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 5) { // Friday before 19
+            deliveryMessage = monday;
+        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 5) { // Friday after 19
+            deliveryMessage = tuesday;
+        } else if (thehours >= 0 && thehours < 24 && dayOfWeek == 6) { // Sat
+            deliveryMessage = tuesday;
+        } else if (thehours >= 0 && thehours < 24 && dayOfWeek == 0) { // Sun 
+            deliveryMessage = tuesday;
+        }
+
+        deliveryTime.textContent = deliveryMessage;
+        deliveryDateDesktop.textContent = deliveryMessage;
+        deliveryDateMobile.textContent = deliveryMessage;
+    }
+
+    deliveryTrashold.textContent = deliveryMessageCZ;
 
       /* Shopify Code Start */
       (function () {
@@ -687,41 +725,5 @@ switch (href) {
     })
     }
     })();
-
-    var tomorrow = "zítra u Vás (Zásilkovna)";
-    var nexttomorrow = "pozítří u Vás (Zásilkovna)";
-    var monday = "v pondělí u Vás (Zásilkovna)";
-    var tuesday = "v úterý u Vás (Zásilkovna)";
-
-    if (thehours >= 0 && thehours < 19 && dayOfWeek == 1) {
-        deliveryMessage = tomorrow;
-    } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 1) {
-        deliveryMessage = nexttomorrow;
-    } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 2) {
-        deliveryMessage = tomorrow;
-    } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 2) {
-        deliveryMessage = nexttomorrow;
-    } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 3) {
-        deliveryMessage = tomorrow;
-    } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 3) {
-        deliveryMessage = nexttomorrow;
-    } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 4) { // Thursday before 19
-        deliveryMessage = tomorrow;
-    } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 4) { // Thursday after 19
-        deliveryMessage = monday;
-    } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 5) { // Friday before 19
-        deliveryMessage = monday;
-    } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 5) { // Friday after 19
-        deliveryMessage = tuesday;
-    } else if (thehours >= 0 && thehours < 24 && dayOfWeek == 6) { // Sat
-        deliveryMessage = tuesday;
-    } else if (thehours >= 0 && thehours < 24 && dayOfWeek == 0) { // Sun 
-        deliveryMessage = tuesday;
-    }
-
-    deliveryTrashold.textContent = deliveryMessageCZ;
-    deliveryTime.textContent = deliveryMessage;
-    deliveryDateDesktop.textContent = deliveryMessage;
-    deliveryDateMobile.textContent = deliveryMessage;
 
 };
