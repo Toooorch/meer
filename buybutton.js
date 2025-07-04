a8f9b6f82f58421f36babcb2e9ca2af87ac096e3
Michal Soukup <michal@michalsoukup.com>
2025-07-04 14:06:05 +0200

    Slow mode
    Slow mode

--- buybutton.js
+++ buybutton.js
@@ -941,30 +941,20 @@ break;
 
   default:
       // Czech
-        var tomorrow = "zítra u Vás (Zásilkovna)";
-        var nexttomorrow = "pozítří u Vás (Zásilkovna)";
-        var monday = "v pondělí u Vás (Zásilkovna)";
-        var tuesday = "v úterý u Vás (Zásilkovna)";
+        var tomorrow = "zítra u Vás";
+        var nexttomorrow = "pozítří u Vás";
+        var monday = "v pondělí u Vás";
+        var tuesday = "v úterý u Vás";
 
-        if (thehours >= 0 && thehours < 19 && dayOfWeek == 1) {
-            deliveryMessage = tomorrow;
-        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 1) {
+        if (thehours >= 0 && thehours < 24 && dayOfWeek == 1) { // Mon
             deliveryMessage = nexttomorrow;
-        } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 2) {
-            deliveryMessage = tomorrow;
-        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 2) {
+        } else if (thehours >= 0 && thehours < 24 && dayOfWeek == 2) { // Tue
             deliveryMessage = nexttomorrow;
-        } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 3) {
-            deliveryMessage = tomorrow;
-        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 3) {
+        } else if (thehours >= 0 && thehours < 24 && dayOfWeek == 3) { // Wed
             deliveryMessage = nexttomorrow;
-        } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 4) { // Thursday before 19
-            deliveryMessage = tomorrow;
-        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 4) { // Thursday after 19
+        } else if (thehours >= 0 && thehours < 24 && dayOfWeek == 4) { // Thu
             deliveryMessage = monday;
-        } else if (thehours >= 0 && thehours < 19 && dayOfWeek == 5) { // Friday before 19
-            deliveryMessage = monday;
-        } else if (thehours >= 19 && thehours < 24 && dayOfWeek == 5) { // Friday after 19
+        } else if (thehours >= 0 && thehours < 24 && dayOfWeek == 5) { // Fri
             deliveryMessage = tuesday;
         } else if (thehours >= 0 && thehours < 24 && dayOfWeek == 6) { // Sat
             deliveryMessage = tuesday;
